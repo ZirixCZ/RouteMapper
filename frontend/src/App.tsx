044e2c99e1b5116f7afc24b5e4e4d236d5e40d6a
ZirixCZ <zirixcz@gmail.com>
2022-10-13 11:22:24 +0200

    feat: navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
+import "./assets/App.css";
 import React from 'react';
+import PlusSquare from "./assets/svg/plus-square.svg";
+import PersonCircle from "./assets/svg/person-circle.svg";
 import {Routes, Route} from "react-router-dom";
 import Feed from "./pages/Feed/Feed";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/test" element={<Feed/>} />
-    </Routes>
+      <div className="container">
+          <div className="navbar">
+              <div className="navbarStart"><h3>RoadMapper</h3></div>
+              <div className="navbarCenter">
+                  <h3 className="feed">Feed</h3>
+                  <h3 className="divider">|</h3>
+                  <h3 className="map">Mapa</h3>
+              </div>
+              <div className="navbarEnd">
+                  <h3>Nahlásit</h3>
+                  <img src={PlusSquare} alt="plus square icon" />
+                  <img src={PersonCircle} alt="person circle icon" />
+              </div>
+          </div>
+          <Routes>
+              <Route path="/test" element={<Feed/>} />
+          </Routes>
+      </div>
+
   );
 }
 
