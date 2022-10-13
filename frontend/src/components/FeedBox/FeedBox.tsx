35968490636789a1c9fbeba2372bbcf2cdce9820
Tomáš Diblík <dibla.tomas@post.cz>
2022-10-13 16:26:58 +0200

    [Fix] alsdkjf

--- frontend/src/components/FeedBox/FeedBox.tsx
+++ frontend/src/components/FeedBox/FeedBox.tsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowUp from "../../assets/svg/arrowUp.svg";
 import ArrowDown from "../../assets/svg/arrowDown.svg";
 import Line from "../../assets/svg/Line.svg";
 import styles from "./FeedBox.module.css";
 
 const FeedBox = (props: any) => {
+  const [downvote, setDownvote] = useState(props.downvotes ?? 0);
+  const [upvotes, setUpvotes] = useState(props.upvotes ?? 0);
 
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <img src={props.img} alt="vymol" />
-            </div>
-            <div className={styles.paragraphContainer}>
-                <p className={styles.paragraph}>{props.description}</p>
-                <div className={styles.buttonContainer}>
-                    <img src={ArrowUp} alt="upvote button" />
-                    <p>{props.downvotes}</p>
-                    <img src={ArrowDown} alt="downvote button" />
-                    <p>{props.upvotes}</p>
-                </div>
-            </div>
-            <div className={styles.divider}>
-                <img src={Line} alt="linka" />
-            </div>
+  const downVote = () => {
+    setDownvote(downvote - 1);
+  };
+
+  const upvoted = () => {
+    setUpvotes(upvotes + 1);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <img src={props.img} alt="vymol" />
+      </div>
+      <div className={styles.paragraphContainer}>
+        <p className={styles.paragraph}>{props.description}</p>
+        <div className={styles.buttonContainer}>
+          <img
+            onClick={() => upvoted()}
+            style={{ cursor: "pointer" }}
+            src={ArrowUp}
+            alt="upvote button"
+          />
+          <p>{upvotes}</p>
+          <img
+            onClick={() => downVote()}
+            src={ArrowDown}
+            style={{ cursor: "pointer" }}
+            alt="downvote button"
+          />
+          <p>{downvote}</p>
         </div>
-    )
-
-}
+      </div>
+      <div className={styles.divider}>
+        <img src={Line} alt="linka" />
+      </div>
+    </div>
+  );
+};
 
 export default FeedBox;
