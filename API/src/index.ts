2f7edc179e6d0ba4d38b553f7fa1d7b64e765963
Tomáš Diblík <dibla.tomas@post.cz>
2022-10-13 16:18:43 +0200

    [Feature] Make uploading posts functional

--- API/src/index.ts
+++ API/src/index.ts
@@ -10,8 +10,9 @@ const app = express();
 const port = 8080;
 
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
-const jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json({ limit: "50mb" });
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
