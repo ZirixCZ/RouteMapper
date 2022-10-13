9cac44b60f905d7580c95c9d4cc9105f9295a44f
ZirixCZ <zirixcz@gmail.com>
2022-10-13 14:59:25 +0200

    feat: create post enhancements

--- frontend/src/pages/CreatePost/CreatePost.tsx
+++ frontend/src/pages/CreatePost/CreatePost.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
 import ArrowLeft from "../../assets/svg/arrow.svg";
+import Map from "../../assets/svg/Group 3.svg";
+import Image from "../../assets/svg/Group 4.svg";
 import styles from "./CreatePost.module.css";
 
 const CreatePost = () => {
@@ -12,11 +14,11 @@ const CreatePost = () => {
             <div className={styles.wrapper}>
                 <img src={ArrowLeft} alt="sipka ukazujici doleva" onClick={() => navigate("/feed")} style={{cursor: 'pointer'}} />
                 <div className={styles.postWrapper}>
-                    <div className={styles.postPhoto}></div>
-                    <div className={styles.postMap}></div>
+                    <div className={styles.postPhoto}><img src={Image} alt="vyberte obrazek" /></div>
+                    <div className={styles.postMap}><img src={Map} alt="vyberte z mapy" /></div>
                 </div>
                 <div className={styles.bottomContainer}>
-                    <textarea placeholder="Zadejte popisek problému..."></textarea>
+                        <textarea placeholder="Zadejte popisek problému..."></textarea>
                     <div className={styles.bottomContainerButtonWrapper}>
                         <button>Přidat</button>
                     </div>
